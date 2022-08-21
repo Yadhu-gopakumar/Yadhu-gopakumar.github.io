a489ed4acb1956c20ce339c8cddc9032e038ad12
Yadhu gopakumar <87005083+Yadhu-gopakumar@users.noreply.github.com>
2022-08-21 16:04:38 +0530

    tic tac toe game using vanila js

--- tic_tac_toe/script.js
+++ tic_tac_toe/script.js
@@ -1,17 +1,31 @@
 const start_btn = document.getElementById('start_btn');
 const start_display = document.getElementById('start_display');
 win_flag = false;
+const winAudio = new Audio('assets/win.wav');
+const drwAudio = new Audio('assets/draw.wav');
+const clickAudio = new Audio('assets/click.wav');
+
 start_btn.addEventListener('click', () => {
     start_display.style.display = 'none';
+clickAudio.play();
 })
 
+
 var box = document.querySelectorAll(".box");
-const tgle_btn = document.getElementById("toggler");
+
+var cell = document.getElementById('box');
 // [0][1][2]
 // [3][4][5]
 // [6][7][8]
 let win_combos = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 
 ];
 
@@ -34,25 +48,42 @@ box.forEach(function (selected_box) {
     selected_box.addEventListener('click', () => {
 
         if (selected_box.innerHTML == "") {
-
+            clickAudio.play();
             selected_box.innerHTML = current_player;
+            togglePlay(current_player, selected_box);
+
             filledBox++;
             if (filledBox == 9) {
-                filledBox=0;
+                filledBox = 0;
                 drawmsg();
             }
             check_winner();
-            current_player = current_player == "x" ? "o" : "x";
-            // if (current_player == "x") {
-            //     tgle_btn.style.left = "3px";
-            // }
-            // else {
-            //     tgle_btn.style.right = '3px';
-            // }
-        }
 
+            current_player = (current_player == "x") ? "o" : "x";
+
+        }
     })
 });
+//chance toggler function
+var tgle_btn = document.getElementById("toggler");
+
+function togglePlay(current_player, selected_box) {
+
+    if (current_player == "x") {
+
+        tgle_btn.style.left = "3px";
+        selected_box.style.color = 'red';
+    }
+    if (current_player == "o") {
+
+        tgle_btn.style.right = '3px';
+        selected_box.style.color = ' rgb(0, 153, 255)';
+
+
+    }
+    return current_player;
+
+}
 //restart button function
 const restart = document.getElementById("restart");
 
@@ -63,35 +94,83 @@ restart.addEventListener('click', () => {
 })
 //clearing board
 function clrBoard() {
-    filledBox=0;
+    let strike = document.querySelector('.strike');
+    strike.style.display = 'none';
+    strike.style.transform = 'rotate(0deg)';
+    strike.style.top = '150px';
+    strike.style.left = '7px';
+    filledBox = 0;
     for (let i = 0; i <= 9; i++) {
         box[i].innerHTML = "";
 
     }
+
 }
 
 //winning msg display
 function winmsg(combination, current_player) {
-    combination.forEach(function (index) {
-        box[index].classList.add('highlight');
-        win_display = document.getElementById("win_display");
-        restart_btn = document.getElementById("restart_btn");
+winAudio.play();
+    let strike = document.querySelector('.strike');
+
+    if (combination == win_combos[0]) {
+        strike.style.display = 'block';
+        strike.style.top = '50px';
+    }
+    if (combination == win_combos[1]) {
+        strike.style.display = 'block';
+    }
+    if (combination == win_combos[2]) {
+        strike.style.display = 'block';
+        strike.style.top = '250px';
+    }
+    if (combination == win_combos[3]) {
+        strike.style.display = 'block';
+        strike.style.right = '100px';
+        strike.style.transform = 'rotate(90deg)';
+    }
+    if (combination == win_combos[4]) {
+        strike.style.display = 'block';
+        strike.style.transform = 'rotate(90deg)';
+    }
+    if (combination == win_combos[5]) {
+        strike.style.display = 'block';
+        strike.style.left = '100px';
+        strike.style.transform = 'rotate(90deg)';
+    }
+    if (combination == win_combos[6]) {
+        strike.style.display = 'block';
+        strike.style.transform = 'rotate(45deg)';
+    }
+    if (combination == win_combos[7]) {
+        strike.style.display = 'block';
+        strike.style.transform = 'rotate(135deg)';
+    }
+
+    let win_display = document.getElementById("win_display");
+    var restart_btn = document.getElementById("restart_btn");
 
 
-        win_msg = document.getElementById("win_msg");
-        win_msg.innerHTML = current_player + "-won";
+    var win_msg = document.getElementById("win_msg");
+    win_msg.innerHTML = current_player + "-won";
 
+
+    setTimeout(() => {
         win_display.style.display = "flex";
-        restart_btn.addEventListener('click', () => {
-            win_display.style.display = 'none';
-        })
         clrBoard();
+
+    }, 1000);
+    restart_btn.addEventListener('click', () => {
+        win_display.style.display = 'none';
+
     })
 }
+
 //display msg
 function drawmsg() {
     filledBox = 0;
-    win_msg = document.getElementById("win_msg");
+    drwAudio.play();
+    var win_msg = document.getElementById("win_msg");
+
     win_msg.innerHTML = "Draw!!!";
 
     win_display.style.display = "flex";
